9dc0c288507a2a96757beff8c51fc7c81b6872a3
Samuel Nogueira <tiosam450@hotmail.com>
2024-11-24 08:49:16 -0400

    Desk Casa

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex">
+      <h1>TESTE</h1>
+    </main>
+  )
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,8 @@
+
+export function Header(){
+  return(
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <h1>TESTE</h1>
+    </header>
+  )
+}
